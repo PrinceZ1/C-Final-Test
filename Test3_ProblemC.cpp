556d3f02c417a12df6ca6d84dad119aed921f0df
PrinceZ1 <88963977+PrinceZ1@users.noreply.github.com>
2022-12-04 12:02:06 +0700

    Update Test3_ProblemC.cpp

--- Test3_ProblemC.cpp
+++ Test3_ProblemC.cpp
@@ -14,7 +14,8 @@ N dòng tiếp theo mỗi dòng gồm M phần tử
 
 Dòng tiếp theo là Q : số lượng truy vấn
 
-Q dòng tiếp theo mô tả truy vấn gồm 4 số : số thứ nhất là loại truy vấn (1 hoặc 2), nếu truy vấn loại 1 3 số tiếp theo sẽ là x, y, z, nếu là truy vấn loại 2 thì 3 số tiếp theo sẽ là u, v, t.
+Q dòng tiếp theo mô tả truy vấn gồm 4 số : số thứ nhất là loại truy vấn (1 hoặc 2), 
+nếu truy vấn loại 1 3 số tiếp theo sẽ là x, y, z, nếu là truy vấn loại 2 thì 3 số tiếp theo sẽ là u, v, t.
 
 Constraints
 
